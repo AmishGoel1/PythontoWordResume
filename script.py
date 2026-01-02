7d5cd90817ccce48b065ff40825943dee4eff47b
Amish Goel <amishgoel1@icloud.com>
2026-01-01 20:26:52 -0800

    Added sections in yaml file and added code for some sections in script.py

--- script.py
+++ script.py
@@ -1,11 +1,18 @@
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt
+import yaml
+
+with open('points.yaml', 'r') as f:
+    data = yaml.safe_load(f)
 
 name = 'AMISH GOEL'
 email = 'Jobs@lets.workwithamish.me'
 linkedinLink = 'https://www.linkedin.com/in/amish-goyal-096066b4/'
 github = 'https://github.com/AmishGoel1'
+
+sections = ('Professional Summary', 'Core Skills', 'Work Experience', 'Education & Certificates', 'Projects')
+
 doc = Document()
 
 nametext = doc.add_paragraph('AMISH GOEL')
@@ -19,4 +26,29 @@ linkspara = doc.add_paragraph(f"{email} | LinkedIn | GitHub")
 linkspara.runs[0].font.name = 'Times New Roman'
 linkspara.runs[0].font.size = Pt(14)
 linkspara.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+for section in sections:
+    currentsection = doc.add_paragraph(section)
+    currentrun = currentsection.runs[0]
+    currentrun.font.name = 'Times New Roman'
+    currentrun.font.size = Pt(13)
+
+    if section == 'Professional Summary':
+        summarytext = doc.add_paragraph(f"\n{data['summary']}")
+        summarytext.runs[0].font.size = Pt(12)
+        summarytext.runs[0].font.name = 'Times New Roman'
+    
+    elif section == 'Core Skills':
+        for skill in data['skills']:
+           for key, value in skill.items():
+                doc.add_paragraph(f"{key}: {value}", style='List Bullet')
+
+    elif section == 'Projects':
+        for project in data['projects']:
+           for key, value in project.items():
+                doc.add_paragraph(key)
+                for point in value:
+                    doc.add_paragraph(f"{point}", style='List Bullet')
+
 doc.save('sapmle1.docx')
+
