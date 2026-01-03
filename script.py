6fb5cc9edac18a1a50b55e7520f396f74dab8229
Amish Goel <amishgoel1@icloud.com>
2026-01-02 21:08:07 -0800

    Corrected after spacing and bold formatting for section headings

--- script.py
+++ script.py
@@ -28,25 +28,32 @@ linkspara.runs[0].font.size = Pt(14)
 linkspara.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 for section in sections:
-    currentsection = doc.add_paragraph(section)
+    currentsection = doc.add_paragraph(f"{section}\n")
     currentrun = currentsection.runs[0]
     currentrun.font.name = 'Times New Roman'
     currentrun.font.size = Pt(13)
-
+    currentrun.bold= True
+    currentsection.paragraph_format.space_after = Pt(0)
+    
     if section == 'Professional Summary':
-        summarytext = doc.add_paragraph(f"\n{data['summary']}")
+        summarytext = doc.add_paragraph(f"{data['summary']}")
+        
         summarytext.runs[0].font.size = Pt(12)
         summarytext.runs[0].font.name = 'Times New Roman'
+        
+        # summarytext.paragraph_format.line_spacing = 1.05
     
     elif section == 'Core Skills':
         for skill in data['skills']:
            for key, value in skill.items():
-                doc.add_paragraph(f"{key}: {value}", style='List Bullet')
+                doc.add_paragraph(f"{key}: {value}")
     
     elif section == 'Work Experience':
         for work in data['work']:
             for key, value in work.items():
                 doc.add_paragraph(f"{value[0]['Title']}, {value[1]['Company']} {value[2]['Date']}")
+                for point in value[3]['Points']:
+                    doc.add_paragraph(f"{point}", style='List Bullet')
 
     elif section == 'Projects':
         for project in data['projects']:
