f87ec0173cb92eb280a5eec18be5de7164435922
Amish Goel <amishgoel1@icloud.com>
2026-01-05 14:41:30 -0800

    Cleaned up yaml even more and added Pydantic classes in script for validation

--- script.py
+++ script.py
@@ -3,11 +3,48 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt, Inches
 from docx.text.run import Run
 from docx.text.paragraph import Paragraph
+from pydantic import BaseModel
+from typing import List
 import yaml
 
 with open('points.yaml', 'r') as f:
     data = yaml.safe_load(f)
 
+class skillsclass(BaseModel):
+    category: str
+    skill: str
+
+class workclass(BaseModel):
+    Title: str
+    Company: str
+    Date: str
+    Points: List[str]
+
+class projectclass(BaseModel):
+    Name: str
+    Points: List[str]
+
+class certificateclass(BaseModel):
+    Name: str
+    Issuer: str
+
+class credentialclass(BaseModel):
+    Name: str
+    Points: List[str]
+
+class educationclass(BaseModel):
+    Name: str
+    Credential: List[credentialclass]
+
+class Resume(BaseModel):
+    summary: str
+    skills: List[skillsclass]
+    education: List[educationclass]
+    certificates: List[certificateclass]
+    work: List[workclass]
+    projects: List[projectclass]
+
+resume = Resume.model_validate(data)
 name = 'AMISH GOEL'
 email = 'Jobs@lets.workwithamish.me'
 linkedinLink = 'https://www.linkedin.com/in/amish-goyal-096066b4/'
@@ -40,62 +77,64 @@ for section in sections:
     formatting(currentsection.runs[0], True, Pt(13))
     spacing(currentsection, Pt(0), Pt(0))
     if section == 'Professional Summary':
-        summarytext = doc.add_paragraph(f"{data['summary']}\n")
+        summarytext = doc.add_paragraph(f"{resume.summary}\n")
         formatting(summarytext.runs[0], False)     
         spacing(summarytext, Pt(0), Pt(0))
     
     elif section == 'Core Skills':
-        for key, value in data['skills'][0].items():
-            currentpoint = doc.add_paragraph(f"{key}: ")
+        for skill in resume.skills:
+            currentpoint = doc.add_paragraph(f"{skill.category}: ")
             formatting(currentpoint.runs[0])
-            valuerun = currentpoint.add_run(f"{value}")
-            formatting(valuerun, False)
-            currentpoint.paragraph_format.space_after = Pt(3)
+            for i, point in enumerate(skill.skill):
+                if i == len(skill.skill) - 1:
+                    valuerun = currentpoint.add_run(f"{point}")
+                else:
+                    valuerun = currentpoint.add_run(f"{point}, ")
+                formatting(valuerun, False)
+                currentpoint.paragraph_format.space_after = Pt(3)
         doc.add_paragraph()
     
     elif section == 'Education & Certificates':
-        for university in data['education']:
-                universityname = doc.add_paragraph(university['Name'])
+        for university in resume.education:
+                universityname = doc.add_paragraph(university.Name)
                 formatting(universityname.runs[0], True)
-                for credential in university['Credential']:
-                        credentialname = doc.add_paragraph(credential['Name'])
+                for credential in university.Credential:
+                        credentialname = doc.add_paragraph(credential.Name)
                         credentialname.paragraph_format.space_after = Pt(0)
                         formatting(credentialname.runs[0], True)
-                        for point in credential['Points']:
+                        for point in credential.Points:
                             currentpoint = doc.add_paragraph(point, style='List Bullet')
                             formatting(currentpoint.runs[0], False)
                             currentpoint.paragraph_format.space_after = Pt(0)
         doc.add_paragraph()
-        for certificate in data['certificates']:
-            for key, value in certificate.items():
-                certificatename = doc.add_paragraph(value, style='List Bullet')
-                formatting(certificatename.runs[0], False)
-                certificatename.paragraph_format.space_after = Pt(2)
+        for certificate in resume.certificates:
+            certificatename = doc.add_paragraph(f"{certificate.Name}: {certificate.Issuer}", style='List Bullet')
+            formatting(certificatename.runs[0], False)
+            certificatename.paragraph_format.space_after = Pt(2)
 
     elif section == 'Work Experience':
-        for work in data['work']:
-            doc.add_paragraph(f"{work['Title']}, {work['Company']} {work['Date']}")
-            for point in work['Points']:
+        for work in resume.work:
+            doc.add_paragraph(f"{work.Title}, {work.Company} {work.Date}")
+            for point in work.Points:
                 currentworkpoint = doc.add_paragraph(f"{point}", style='List Bullet')
                 formatting(currentworkpoint.runs[0], False)
                 currentworkpoint.paragraph_format.space_after = Pt(3)
             doc.add_paragraph()
     
     elif section == 'Projects':
-        for key, value in data['projects'][0].items():
-            projectname = doc.add_paragraph(key)
+        for project in resume.projects:
+            projectname = doc.add_paragraph(project.Name)
             formatting(projectname.runs[0])
-            if key == 'Project 1':
+            if project.Name == 'Project 1 Name':
                 projectname.paragraph_format.space_before = Pt(0)
             else: 
                 projectname.paragraph_format.space_before = Pt(9)
-            for point in value:
+            for point in project.Points:
                 currentpoint = doc.add_paragraph(f"{point}", style='List Bullet')
                 formatting(currentpoint.runs[0], False)
                 spacing(currentpoint, Pt(15), Pt(3))
 
 section = doc.sections[0]
-
 section.left_margin = Inches(0.5)
 section.right_margin = Inches(0.5)
 section.top_margin = Inches(0.437)
